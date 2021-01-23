b7e0c96f19de662f207bb419e219300bd44771b7
gitsxb <502406732@qq.com>
2021-01-23 22:30:37 +0800

    11111

--- src/main/java/cn/run/wait/controller/IndexController.java
+++ src/main/java/cn/run/wait/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
-    @RequestMapping("/index")
+    @RequestMapping("/index23")
     public String toIndex(){
         return "index1111111";
     }
